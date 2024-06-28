910378422987db4dc4ae1933bef185f165354679
Philippe Llerena <philippe.llerena@gmail.com>
2024-06-28 19:04:57 +0200

    feat: Add rer-resolver and rer-version crates
    feat: add version and solving protype
    
    fix range desc
    
    before clippy
    
    add clippy autofix
    
    add negate
    
    fix: fix superset case
    
    remove unused
    
    quality: change name of function to be less ambiguious
    
    fix: data_set path
    
    quality: add anchor to remove warning
    
    feat: add fast return
    
    parse solutions
    
    chore: change Struct name
    
    Fix typo in crate dependency name
    
    chore: add an alias to the lib
    
    feat: add candidate selector
    
    add integration test and fix to it
    
    feat: Update resolve function to use package_name variable
    
    chore: exclude the private data_set
    
    chore: add a public dataset
    
    quality: clean unused import
    
    chore: ignore more
    
    chore: clean and add comments
    
    chore: add get_sep() method to VersionRequierement
    
    feat: Update candidate selector to find candidates within a range
    
    Update dependencies in rer-resolver crate
    
    chore: Refactor code and improve test coverage

---
+++ crates/examples/with_candidate_selector_rayon.rs
@@ -0,0 +1,19 @@
+use rer_resolver::{solver,LocalPackages};
+use std::env;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = &args[1];
+    if input.is_empty() {
+        eprintln!("Package name can't be empty");
+        std::process::exit(1);
+    }
+    let requirements = args[1..].iter().map(|x| x.as_str()).collect();
+    let packages = LocalPackages::build_from_json_path("data_set_private/rez_lib.json");
+    let start = std::time::Instant::now();
+    let solution = solver(requirements, packages);
+    let elapsed = start.elapsed();
+    println!("Time: {:?}", elapsed);
+    println!("{:#?}", solution);
+}
