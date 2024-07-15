2b246b76490cd53e43e87bc355b10231a513eb94
Philippe Llerena <philippe.llerena@gmail.com>
2024-07-15 10:34:16 +0200

    feat: Add serde crate as a dependency
    Add the serde crate as a dependency in the Cargo.toml files. This crate is needed for serialization and deserialization purposes and will be used in the project.

--- crates/examples/rez_benchmark.rs
+++ crates/examples/rez_benchmark.rs
@@ -1,19 +1,30 @@
 use rer_resolver::solver;
 use serde_json;
+use serde::Deserialize;
 use std::fs;
 
+#[derive(Deserialize)]
+struct RezResolveBenchmark {
+    request: Vec<String>,
+    status: String,
+    resolve_time: f32,
+    resolved_packages: Option<Vec<String>>,
+}
+
 fn main() {
-    let data_str =
+    let data_str_request =
         fs::read_to_string("data_set_private/requests.json").expect("Unable to read file");
-    let requests: Vec<Vec<&str>> = serde_json::from_str(&data_str).unwrap();
-    for requirements in requests {
+    let requests: Vec<Vec<&str>> = serde_json::from_str(&data_str_request).unwrap();
+    let data_str_resolves =
+    fs::read_to_string("data_set_private/resolves.json").expect("Unable to read file");
+    let resolves: Vec<RezResolveBenchmark> = serde_json::from_str(&data_str_resolves).unwrap();
+    for resolve in resolves {
         let paths = vec![std::path::PathBuf::from(
             "/home/philippe.llerena/workspace/github.com/doubleailes/rer-bkp/data_set/packages",
         )];
         let start = std::time::Instant::now();
-        let solution = solver(requirements, paths);
+        let solution = solver(resolve.request.iter().map(|x|x.as_str() ).collect(), paths);
         let elapsed = start.elapsed();
-        println!("Resolve in Time: {:?}", elapsed);
-        println!("{:#?}", solution);
+        println!("Resolve in Time: {:?} rez resolve it in {} ms", elapsed, resolve.resolve_time*1000.0);
     }
 }
